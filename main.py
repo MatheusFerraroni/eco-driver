876471aa4fe849e754fde119544eb54a1f9c871c
MatheusF <matheussanches531@gmail.com>
2020-07-22 22:33:31 -0300

    ja acontece algumas simulações.. tem um bug que o sumo não para direito :(

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import os
 import sys
 from datetime import datetime
 import threading
-
+import socket
+import subprocess
+import time
 # from ia.model import Model
 # import pandas as pd
 
@@ -55,15 +57,11 @@ def find_unused_port():
     return port
 
 def terminate_sumo(sumo):
-    if sumo.returncode == None:
-        # os.kill(sumo.pid, signal.SIGILL)
-        os.system("taskkill.exe /F /im sumo.exe")
-        time.sleep(1)
-        # if sumo.returncode == None:
-        #     os.kill(sumo.pid, signal.SIGKILL)
-        #     time.sleep(1)
-        #     if sumo.returncode == None:
-        #         time.sleep(10)
+
+    for _ in range(3):
+        if sumo.returncode == None:
+            os.system("taskkill.exe /F /im sumo.exe")
+            time.sleep(1)
 
 
 
@@ -75,58 +73,87 @@ def custom_mutate(index, genome):
         genome[index] = 0
     return genome
 
+def run():
+
+    step = 1
+    
+    traci.route.add("trip", ['A0B0', 'B0C0', 'C0D0', 'D0E0', 'E0F0', 'F0G0', 'G0H0', 'H0I0', 'I0J0', 'J0K0', 'K0L0', 'L0M0', 'M0N0', 'N0O0', 'O0P0', 'P0Q0', 'Q0R0', 'R0S0', 'S0T0'])
+    traci.vehicle.add("caminhao", "trip")
+    traci.vehicle.setParameter("caminhao","carFollowModel","KraussPS")
+    traci.vehicle.setVehicleClass("caminhao","truck")
+    traci.vehicle.setShapeClass("caminhao","truck")
+    traci.vehicle.setEmissionClass("caminhao","HBEFA3/HDV")
+    traci.vehicle.setMaxSpeed("caminhao",2.2) # aprox 8 km/h
 
 
+    total_fuel = 0
 
-# def run(network, begin, end, interval, config_file, versao_i):
+    while step == 1 or traci.simulation.getMinExpectedNumber() > 0:        
+        traci.simulationStep()                          
+        vehicles = traci.simulation.getEndingTeleportIDList()        
+        for vehicle in vehicles:
+            traci.vehicle.remove(vehicle, reason=4)    
 
-#     step = 1
+        # print("getAccel",traci.vehicle.getAccel("caminhao"))
+        # print("getVehicleClass",traci.vehicle.getVehicleClass("caminhao"))
+        # print("getEmissionClass",traci.vehicle.getEmissionClass("caminhao"))
+        # print("getFuelConsumption",traci.vehicle.getFuelConsumption("caminhao"))
 
-#     controlador = Trabalho(config_file, versao_i)
+        try:
+            total_fuel += traci.vehicle.getFuelConsumption("caminhao")
+        except:
+            pass
 
-#     while step == 1 or traci.simulation.getMinExpectedNumber() > 0:
-#         traci.simulationStep()
+        step += 1
+    
 
-#         vehicles = traci.simulation.getEndingTeleportIDList()
-#         for vehicle in vehicles:
-#             traci.vehicle.remove(vehicle, reason=4)
+    print("Simulation finished")
+    traci.close()
+    sys.stdout.flush()
+    time.sleep(1)
 
-#         controlador.do_work(traci, step) # magic is here
+    return total_fuel
 
-#         step += 1
+def start_simulation(sumo, scenario, network, output):
+    unused_port_lock = UnusedPortLock()
+    unused_port_lock.__enter__()
+    remote_port = find_unused_port()
 
-#     controlador.stop()
-
-#     time.sleep(3)
-#     print("Simulation finished")
-#     traci.close()
-#     sys.stdout.flush()
-#     time.sleep(3)
+    sumo = subprocess.Popen([sumo, "-c", scenario, "--tripinfo-output", output, "--device.emissions.probability", "1.0", "--remote-port", str(remote_port), "--duration-log.statistics","--log", "logfile.txt"], stdout=sys.stdout, stderr=sys.stderr)    
+    unused_port_lock.release()
 
 
-# def start_simulation(scenario, network, begin, end, interval, output, config_file, versao_i):
-#     unused_port_lock = UnusedPortLock()
-#     unused_port_lock.__enter__()
-#     remote_port = find_unused_port()
+    try:
+        traci.init(remote_port)            
+        return run()
+    except Exception as e:
+        print(e)
+        terminate_sumo(sumo)
+        unused_port_lock.__exit__()
+        raise
+    finally:
+        print("Terminating SUMO")  
+        terminate_sumo(sumo)
+        unused_port_lock.__exit__()
 
-#     sumo = subprocess.Popen(["sumo-gui", "-c", scenario, "--tripinfo-output", output, "--device.emissions.probability", "1.0", "--remote-port", str(remote_port)], stdout=sys.stdout, stderr=sys.stderr)
-#     unused_port_lock.release()
 
-#     try:
-#         traci.init(remote_port)
-#         run(network, begin, end, interval, config_file, versao_i)
-#     except Exception as e:
-#         print(e)
-#         raise
-#     finally:
-#         print("Terminating SUMO")
-#         terminate_sumo(sumo)
-#         unused_port_lock.__exit__()
+def custom_fitness():
 
+    mapas = ["0.net.xml","1.net.xml","2.net.xml","3.net.xml","4.net.xml","5.net.xml","6.net.xml","7.net.xml","8.net.xml","9.net.xml"]
+    folder = "./mapas/"
+
+    for m in mapas:
+
+        consumo = start_simulation("sumo", (folder+m).replace(".net.xml",".sumo.cfg"), (folder+m), "./output/"+m)
+
+        print(m)
+        print ("CONSUMO", consumo)
 
 
 def main():
 
+    custom_fitness()
+    return
     population_size   = 10
     iteration_limit   = 10
     crossover_type    = 3
@@ -154,11 +181,6 @@ def main():
     # target = targets[dataset_name]
     # target = df.pop(target)
 
-
-    def custom_fitness(genome):
-        return np.random.random()
-        # bool_genome = list(map(bool, genome))
-        # return model.evaluate(df.loc[:, bool_genome].copy(), target)
 
     def custom_random_genome():
         return np.random.random(20)
@@ -198,170 +220,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from __future__ import division
-# import os
-# import sys
-# import subprocess
-# import signal
-# import socket
-# import threading
-# import time
-# from optparse import OptionParser
-# import Simulation
-
-# if 'SUMO_HOME' in os.environ:
-#     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-#     sys.path.append(tools)
-# else:
-#     sys.exit("Environment variable SUMO_HOME not defined")
-
-# sys.path.append(os.path.join('/home/-/sumo-1.5.0/tools'))
-
-# import traci
-
-# class UnusedPortLock:
-#     lock = threading.Lock()
-
-#     def __init__(self):
-#         self.acquired = False
-
-#     def __enter__(self):
-#         self.acquire()
-
-#     def __exit__(self):
-#         self.release()
-
-#     def acquire(self):
-#         if not self.acquired:
-#             UnusedPortLock.lock.acquire()
-#             self.acquired = True
-
-#     def release(self):
-#         if self.acquired:
-#             UnusedPortLock.lock.release()
-#             self.acquired = False
-
-# def find_unused_port():
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-#     sock.bind(('127.0.0.1', 0))
-#     sock.listen(socket.SOMAXCONN)
-#     ipaddr, port = sock.getsockname()
-#     sock.close()
-#     return port
-
-# def terminate_sumo(sumo):
-#     if sumo.returncode == None:
-#         os.kill(sumo.pid, signal.SIGILL)
-#         time.sleep(0.5)
-#         if sumo.returncode == None:
-#             os.kill(sumo.pid, signal.SIGKILL)
-#             time.sleep(1)
-#             if sumo.returncode == None:
-#                 time.sleep(10)
-
-
-# def run(network, begin, end, interval):
-
-#     step = 1
-    
-#     adition = Simulation.Vehicles.add1(traci)    
-    
-#     while step == 1 or traci.simulation.getMinExpectedNumber() > 0:        
-#         traci.simulationStep()                          
-#         vehicles = traci.simulation.getEndingTeleportIDList()        
-#         for vehicle in vehicles:
-#             traci.vehicle.remove(vehicle, reason=4)    
-
-#         step += 1
-    
-#     time.sleep(10)
-#     print("Simulation finished")
-#     traci.close()
-#     sys.stdout.flush()
-#     time.sleep(10)
-        
-# def start_simulation(sumo, scenario, network, begin, end, interval, output):
-#     unused_port_lock = UnusedPortLock()
-#     unused_port_lock.__enter__()
-#     remote_port = find_unused_port()
-
-#     sumo = subprocess.Popen([sumo, "-c", scenario, "--tripinfo-output", output, "--device.emissions.probability", "1.0", "--remote-port", str(remote_port), "--duration-log.statistics","--log", "logfile.txt"], stdout=sys.stdout, stderr=sys.stderr)    
-#     unused_port_lock.release()
-
-
-#     try:
-#         traci.init(remote_port)            
-#         run(network, begin, end, interval)
-#     except Exception as e:
-#         print(e)        
-#         raise
-#     finally:
-#         print("Terminating SUMO")  
-#         terminate_sumo(sumo)
-#         unused_port_lock.__exit__()
-        
-# def main():
-
-#     parser = OptionParser()
-#     parser.add_option("-c", "--command", dest="command", default="sumo-gui", help="The command used to run SUMO [default: %default]", metavar="COMMAND")
-#     parser.add_option("-s", "--scenario", dest="scenario", default="map.sumocfg", help="A SUMO configuration file [default: %default]", metavar="FILE")
-#     parser.add_option("-n", "--network", dest="network", default="map.net.xml", help="A SUMO network definition file [default: %default]", metavar="FILE")    
-#     parser.add_option("-b", "--begin", dest="begin", type="int", default=0, action="store", help="The simulation time (s) at which the re-routing begins [default: %default]", metavar="BEGIN")
-#     parser.add_option("-e", "--end", dest="end", type="int", default=600, action="store", help="The simulation time (s) at which the re-routing ends [default: %default]", metavar="END")
-#     parser.add_option("-i", "--interval", dest="interval", type="int", default=1, action="store", help="The interval (s) of classification [default: %default]", metavar="INTERVAL")    
-#     parser.add_option("-o", "--full-output", dest="output", default="output1.xml", help="The XML file at which the output must be written [default: %default]", metavar="FILE")
-
-
-#     (options, args) = parser.parse_args()
-    
-
-        
-#     start_simulation(options.command, options.scenario, options.network, options.begin, options.end, options.interval, options.output)
-
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
-
-
-
-
-
-
-# class Vehicles:
-
-#     def add1(traci):
-#         traci.route.add("trip", ['3to4', '4to5', '5to6', '6to7', '7to8', '8to9', '9to10', '10to11', '11to12', '12to13', '13to14', '14to15', '15to16', '16to17', '17to18', '18to19', '19to20', '20to21', '21to22', '22to25'])
-#         traci.vehicle.add("caminhao", "trip")
-#         traci.vehicle.setParameter("caminhao","carFollowModel","KraussPS")
-#         traci.vehicle.setVehicleClass("caminhao","truck")
-#         traci.vehicle.setShapeClass("caminhao","truck")
-#         traci.vehicle.setEmissionClass("caminhao","HBEFA3/HDV")
-#         traci.vehicle.setMaxSpeed("caminhao",2.2) # aprox 8 km/h 
-#         print(traci.vehicle.getAccel("caminhao"))
-#         print(traci.vehicle.getVehicleClass("caminhao"))    
-#         print(traci.vehicle.getEmissionClass("caminhao"))       
-
-#         traci.vehicle.add("carrinheiro", "trip")        
-#         traci.vehicle.setVehicleClass("carrinheiro","pedestrian")
-#         traci.vehicle.setShapeClass("carrinheiro","pedestrian")
-#         traci.vehicle.setMaxSpeed("carrinheiro",1) # aprox 4 km/h
-#         traci.vehicle.setEmissionClass("carrinheiro","Zero")
-        
-#         return True
---
+++ mapas/generate.sh
@@ -0,0 +1,20 @@
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 0.net.xml --seed 5379479
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 1.net.xml --seed 6831837
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 2.net.xml --seed 9726976
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 3.net.xml --seed 6106279
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 4.net.xml --seed 9023093
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 5.net.xml --seed 4310387
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 6.net.xml --seed 8351704
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 7.net.xml --seed 8419726
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 8.net.xml --seed 1243412
+
+netgenerate.exe -g --grid.x-number 20 --perturb-x 23 --grid.y-number 2 --perturb-z 100 -o 9.net.xml --seed 5982279
