77e6ae099ab0cd362516d2eae6e4c9f3949fba98
Maria Vitória Rodrigues Oliveira <euvivirodrigues@gmail.com>
2020-08-06 23:34:23 -0300

    update analyzer super

--- analyzer_super.py
+++ analyzer_super.py
@@ -57,7 +57,7 @@ def proc(arquivos):
             valores_fuel.append(float(dado[0][0].attrib["fuel_abs"]))
 
         min_geral = min(min_geral, min(valores_fuel))
-        print(g,min_geral)
+        #print(g,min_geral)
         ys_min_geral.append(min_geral)
         ys_max_geral.append(max(valores_fuel))
         ys_min_geracao.append(min(valores_fuel))
@@ -65,6 +65,8 @@ def proc(arquivos):
         # print(g, min_geral, round(max(valores_fuel),1), round(min(valores_fuel),1), round(mean(valores_fuel),1))
 
     # fig, ax = plt.subplots(figsize=(20,4))
+
+    namePlot = 'total_fuel_consuption_Evolution_during_generations'
     fig, ax = plt.subplots()
     # ax.set_ylim(0,300)
     # ax.patch.set_facecolor('#87d3e0')
@@ -73,15 +75,18 @@ def proc(arquivos):
     ax.plot(geracoes_txt, ys_max_geral,label="Worst Generation")
     ax.plot(geracoes_txt, ys_min_geracao,label="Best Generation")
     ax.plot(geracoes_txt, ys_mean,label="Mean Generation")
-    ax.set_title("Evolution during generations")
-    ax.legend()
-    ax.set_xlabel('Generation')
-    ax.set_ylabel('Total Fuel Consuption')
-    plt.show()
+    #ax.set_title("Evolution during generations")
+    ax.legend(numpoints=1, loc="upper right", ncol=2, fontsize='medium')
+    ax.set_xlabel('Generations')
+    ax.set_ylabel('Total Fuel Consuption (ml/s)')    
+    ax.grid(True, which="both", ls="-", linewidth=0.2, color='0.10', zorder=0)
+    #plt.yscale('log')
+    fig.savefig('plots/'+namePlot+'.png', bbox_inches='tight')
+    #fig.savefig('plots/'+namePlot+'.pdf', bbox_inches='tight')
+    plt.close(fig)
+    #plt.show()
 
-
-
-files = os.listdir("./output")
+files = os.listdir("./output/")
 
 mapas = {
     "super":{"dados":[]}
@@ -91,9 +96,5 @@ for f in files:
     f2 = f.split(".")
 
     mapas[f2[0]]["dados"].append(f)
-
-
-
-
 
 proc(mapas["super"])
